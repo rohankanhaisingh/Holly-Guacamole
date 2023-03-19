104e15dfbb74f77be912e75ad8dc594582f6c710
Rohan Kanhaisingh <tr.kanhaisingh@gmail.com>
2023-03-19 13:52:07 +0100

    Fade-in animatie toegevoegd wanneer het document is geladen

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,7 +1,28 @@
 (function () {
 
 	const app = document.querySelector(".app"),
-		loader = app.querySelector(".app-loader");
+		loader = app.querySelector(".app-loader"),
+		animatingElements = document.querySelectorAll(".can-animate");
+
+	async function animateElements() {
+
+		for (let i = 0; i < animatingElements.length; i++) {
+
+			const element = animatingElements[i];
+
+			element.classList.add("fade-in");
+
+			await waitFor(100);
+		}
+
+		await waitFor(1000);
+
+		animatingElements.forEach(function (em) {
+
+			em.classList.remove("fade-in");
+			em.classList.remove("can-animate");
+		});
+	}
 
 	window.addEventListener("DOMContentLoaded", async function () {
 
@@ -14,5 +35,6 @@
 		loader.classList.remove("fadeout");
 		loader.classList.add("hidden");
 
+		animateElements();
 	});
 })();
