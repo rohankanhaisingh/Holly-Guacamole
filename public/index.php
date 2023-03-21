51aeb7568e8d8263097a637b2e7f0a6c8deba046
Rohan Kanhaisingh <tr.kanhaisingh@gmail.com>
2023-03-21 11:30:20 +0100

    Revert "Correctie?"
    This reverts commit 3404b894fb072ebf56c1214b514c6ebdaa4addd7.

---
+++ public/index.php
@@ -0,0 +1,126 @@
+﻿<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Holly Guacamole - Home pagina</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Style-Type" content="text/css" />
+    <meta name="copyright" content="� W3C" />
+    <meta name="author" lang="en" content="Kay, Jasper, Rohan" />
+    <meta name="robots" content="Thingy" />
+    <meta name="description" content="Holly Guacamole - Home pagina" />
+    <meta name="keyword" content="Holly Guacamole" />
+    <meta name="viewport" content="width=device-width" />
+
+    <link href="./styles/default-styles.css" rel="stylesheet" type="text/css" >
+    <link href="./resources/fonts/fonts.css" rel="stylesheet" type="text/css"/>
+    <link href="./styles/index.css" rel="stylesheet" type="text/css"/>
+    <link href="./styles/navbar.css" rel="stylesheet" type="text/css"/>
+    <link href="./styles/tabs.css" rel="stylesheet" type="text/css"/>
+    <link href="./styles/home.css" rel="stylesheet" type="text/css"/>
+    <link href="./styles/loader.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <div class="app">
+        <div class="container">
+
+            <div class="app-loader">
+                <div class="container">
+                    <div class="absolute-centered loader-contents">
+                        <div class="loader-logo">
+                            <img src="./resources/images/avocado.png" alt="Logo" />
+                        </div>
+                        <div class="loader-text">
+                            <span>Laden van pagina...</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="app-layer app-background">
+                <div class="container">
+                    <video class="app-background-video gekke-hamza-jwz" autoplay muted loop>
+                        <source src="./resources/video/hot-achtergrond.mp4" type="video/mp4" />
+                        <source src="./resources/video/hot-achtergrond.webm" type="video/webm" />
+                        <source src="./resources/video/hot-achtergrond.ogv" type="video/ogg" />
+                    </video>
+                </div>
+            </div>
+
+            <div class="app-layer app-foreground">
+                <div class="container">
+
+                    <div class="app-navbar">
+                        <div class="navbar-logo can-animate">
+                            <img src="./resources/images/avocado.png" alt="Logo" />
+                        </div>
+                        <div class="navbar-description can-animate">
+                            <span>Holly Guacamoley</span>
+                        </div>
+                        <div class="navbar-vl"></div>
+                        <ul class="navbar-links">
+                            <li><a href="?reloaded=true" class="can-animate">Home pagina</a></li>
+                            <li><a href="/kies-tafel.php" class="can-animate">Kies tafel</a></li>
+                            <li><a href="/help.html" class="can-animate">Help</a></li>
+                            <li><a href="/over.html" class="can-animate">Over</a></li>
+                        </ul>
+                        <div class="navbar-hamburger closed">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+
+                    <div class="app-content">
+                        <div class="container scroller">
+
+                            <div class="section first-section">
+                                
+                                <div class="intro-text">
+                                    <h1 class="can-animate">WIEHOEW, JE BENT ER!</h1>
+                                    <h3 class="can-animate">En daar zijn we onwijs blij mee: voel je welkom en neem plaats…</h3>
+                                    <div class="horizontal-line can-animate"></div>
+                                    <p>
+                                        <span class="can-animate">
+                                            Samen gaan we voor een geweldig gezellige avond. Samen zorgen we voor mooie herinneringen om nooit meer te vergeten!
+                                        </span>
+                                        <br />
+                                        <br />
+                                        <span class="can-animate">
+                                            Voor jou zetten we graag wat stappen extra. Of doen we een pirouette, wat jij wilt. Want jouw wensen staan bij ons centraal.
+                                        </span>
+                                        <br />
+                                        <br />
+                                        <span class="can-animate">
+                                            Tapas of à la carte, groot of klein, thuis of bij ons op de zaak. Met onze spannende smaakcombinaties en kwaliteitsingrediënten, kom je bij ons niets tekort en laten we je keer op keer versteld staan.
+
+                                        </span>
+                                        <br />
+                                        <br />
+                                        <span class="can-animate">
+                                            Onze medewerkers maken daarin het verschil. We kiezen ervoor om enkel te werken met vrolijke mensen, die weten dat gezelligheid, gastvrijheid en hard werken samen gaan.
+                                            Wij gaan voor een lach op jouw gezicht, pas dan zijn wij tevreden.
+                                        </span>
+                                        <br />
+                                        <br />
+                                        <i class="can-animate">Holly-Guacamoley Grenzeloos genieten!</i>
+                                        <a href="./kies-tafel.php">
+                                            <div class="button can-animate">
+                                                <span>Kies tafel</span>
+                                            </div>
+                                        </a>
+                                    </p>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="./scripts/utils/waitfor.js" type="text/javascript" defer></script>
+    <script src="./scripts/home.js" type="text/javascript" defer></script>
+</body>
+</html>
