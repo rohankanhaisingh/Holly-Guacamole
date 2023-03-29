280789f0549ae06f27230f55226601e916db7a64
Rohan Kanhaisingh <tr.kanhaisingh@gmail.com>
2023-03-29 13:31:13 +0200

    Specifieke data word uitgelezen indien er een post request word gedaan. Die data word dan op de database opgeslagen

--- server/add-item.php
+++ server/add-item.php
@@ -16,10 +16,12 @@
 	$content = trim(file_get_contents("php://input"));
 	$decodedArray = json_decode($content, true);
 
-	$timestamp = $decodedArray["timestamp"];
-	$text = $decodedArray["text"];
+	$productName = $decodedArray["productName"];
+	$productId = $decodedArray["productId"];
+	$productType = $decodedArray["productType"];
+	$productType = $decodedArray["timestamp"];
 
-	$query = "INSERT INTO bestelregel(Product, Aantal, ProductID, IsVerwerkt) VALUES ('Sex', 13, 234, 1)";
+	$query = "INSERT INTO bestelregel(Product, Aantal, ProductID, IsVerwerkt) VALUES ('$productName', 1, $productId, 0)";
 
     mysqli_query($verbinding, $query);
 
