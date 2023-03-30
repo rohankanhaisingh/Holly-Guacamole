9e29982feee250f4e219792df256c53ed8baf261
bstoffels <87460@roc-teraa.nl>
2023-03-30 10:55:58 +0200

    bestellingen.php aangepast

--- public/bestellingen.php
+++ public/bestellingen.php
@@ -1,41 +1,8 @@
-	function generateProductItem(itemName, itemThumbnail, itemPrice, itemDescription, itemOptionalDescription) {
-	productsGridContainer.innerHTML += 
-		<div class="product-container product-item">
-            <div class="product-item-thumbnail">
-                <img src="${itemThumbnail}" alt="Zoetzure kip" />
-            </div>
-            <div class="product-item-title">
-                <span class="title">${itemName}</span>
-                <div class="price">€${itemPrice}</div>
-            </div>
-            <div class="product-item-description">
-                <span>${itemDescription}</span>
-            </div>
-            <div class="product-item-horizontal-line"></div>
-            <div class="product-item-optional-description">
-                <span>${itemOptionalDescription}</span>
-            </div>
-            <div class="product-item-buttons">
-                <button class="product-item-buttons-button">
-                    <img src="./resources/icons/icon-add.png" alt="Voeg product toe aan winkelmand" />
-                </button>
-                <div class="product-item-buttons-button">
-                    <img src="./resources/icons/icon-compare.png" alt="Vergelijk product met een ander" />
-                </div>
-                <div class="product-item-buttons-button">
-                    <img src="./resources/icons/icon-slider.png" alt="Product samenstellen" />
-                </div>
-                <div class="product-item-buttons-button">
-                    <img src="./resources/icons/icon-info.png" alt="Product informatie" />
-                </div>
-            </div>
-        </div>
-    ;`
-}
+<?php
 function insertBestelRegel() {
     // Voer hier de SQL-query uit om de bestelregel in te voegen in de database 
-        $query = "INSERT INTO bestelregel (Product, Aantal, BestellingID, ProductID, IsVerwerkt) SELECT '', '1', BestellingID, '', '0' FROM bestelling WHERE BestellingID = '';";
-        mysqli_query($connection, $query);
+    $query = "INSERT INTO bestelregel (Product, Aantal, BestellingID, ProductID, IsVerwerkt) SELECT '', '1', BestellingID, '', '0' FROM bestelling WHERE BestellingID = '';";
+    mysqli_query($connection, $query);
     alert('Bestelling is gelukt');
 }
 ?>
